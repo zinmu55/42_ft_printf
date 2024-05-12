a26294b4bac25b276b5ab5e45b797d8be80d2baf
Shintaro Kohtake <skohtake@c2r2s5.42tokyo.jp>
2024-05-12 15:53:50 +0900

    commit identifier process of char type

--- ft_printf.c
+++ ft_printf.c
@@ -6,42 +6,44 @@
 /*   By: skohtake <skohtake@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 08:10:02 by skohtake          #+#    #+#             */
-/*   Updated: 2024/05/12 14:54:58 by skohtake         ###   ########.fr       */
+/*   Updated: 2024/05/12 15:27:40 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 // decide process according identifier
-int	my_process_id(char id, va_list list)
+int	my_process_id(char id, va_list *list)
 {
 	int	res;
 
 	res = 0;
 	if (id == 'c')
 	{
-		res = my_print_char()
+		// res = my_print_char();
+		ft_putchar_fd(((char)va_arg(*list, int)), 1);
+		printf("printf:%c", id);
 	}
-	if (id == 's')
-	{
-	}
-	if (id == 'p')
-	{
-	}
-	if (id == 'd')
-	{
-	}
-	if (id == 'i')
-	{
-	}
-	if (id == '%')
-	{
-	}
+	// if (id == 's')
+	// {
+	// }
+	// if (id == 'p')
+	// {
+	// }
+	// if (id == 'd')
+	// {
+	// }
+	// if (id == 'i')
+	// {
+	// }
+	// if (id == '%')
+	// {
+	// }
 	return (res);
 }
 
 //
-int	my_count_res(char *format, va_list list)
+int	my_count_res(char *format, va_list *list)
 {
 	int	res;
 	int	index;
@@ -68,14 +70,13 @@ int	ft_printf(const char *format, ...)
 {
 	va_list	list;
 	char	*dup;
-	int		index;
 	int		res_len;
 
 	dup = ft_strdup(format);
 	if (dup == NULL)
 		return (0);
 	va_start(list, format);
-	res_len = my_count_res(dup, list);
+	res_len = my_count_res(dup, &list);
 	va_end(list);
 	free(dup);
 	return (res_len);
